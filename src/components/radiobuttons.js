4eafa90ad220652c7908b9a59e930ce41f339794
Olumide Jones <olumideyjones@yahoo.com>
2022-02-14 16:47:37 +0100

    first commit

---
+++ src/components/radiobuttons.js
@@ -0,0 +1,27 @@
+const QuestionCount = (props) => {
+
+  return (
+    <div
+      onChange={props.setQuestionCount}
+    >
+      <label htmlFor="twenty" key="twenty">
+        <input type="radio" name="count" id="twenty" value="twenty" />
+        Stage 1 (20 questions)
+      </label>
+
+
+      <label htmlFor="fifty" key="fifty">
+        <input type="radio" name="count" id="fifty" value="fifty" />
+        Stage 2 (50 questions)
+      </label>
+
+
+      <label htmlFor="hundred" key="hundred">
+        <input type="radio" name="count" id="hundred" value="hundred" />
+        Stage 3 (100 questions)
+      </label>
+    </div>
+  )
+}
+
+export default QuestionCount
